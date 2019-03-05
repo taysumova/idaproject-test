a231ac7e3d7b88d3a251d7b58d0bebc46a65982a
Luiza <luitays@mail.ru>
2019-03-05 17:18:09 +0300

    for prod version

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './js/validation'
 import './js/navMenu'
 
 //styles
+import './scss/main.scss'
 import './scss/dropdown.scss'
 import './scss/navMenu.scss'
 import './scss/footer.scss'
 import './scss/infoDisplay.scss'
 import './scss/formElements.scss'
-import './scss/main.scss'
